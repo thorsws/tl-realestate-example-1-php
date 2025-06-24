19918536533aaf3f75256f4bc398e5695260dfc7
Jan Rosen <janrosen@mac.lan>
2025-06-24 10:10:14 -0700

    Simplify Apache configuration and add test file

---
+++ public/test.php
@@ -0,0 +1,6 @@
+<?php
+echo "Test file is working!<br>";
+echo "REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "<br>";
+echo "SCRIPT_NAME: " . $_SERVER['SCRIPT_NAME'] . "<br>";
+echo "PHP_SELF: " . $_SERVER['PHP_SELF'] . "<br>";
+phpinfo();
